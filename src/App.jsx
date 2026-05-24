702583770c870ba959f6194a894764621d492931
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-24 10:39:00 +0300

    first push starter files

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <div className="text-3xl text-center pt-10">App</div>;
+};
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
